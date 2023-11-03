27bc8fc8ca154c21771459b6109f383414034e5b
Vignesh <joshivignesh@users.noreply.github.com>
2023-11-03 23:56:47 +0530

    Added Controller

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AuctionDbContext>(opt =>{
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
