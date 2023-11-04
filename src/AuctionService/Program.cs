920f23dd8634bf8fabb6ce7de8f506872f1136a2
Vignesh <joshivignesh@users.noreply.github.com>
2023-11-04 23:27:49 +0530

    Added Docker

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -5,7 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AuctionDbContext>(opt =>{
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
