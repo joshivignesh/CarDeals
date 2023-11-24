88409874ccd1d238a0e8235f5a3f1539dcdda183
Vignesh <joshivignesh@users.noreply.github.com>
2023-11-24 22:57:20 +0530

    Added Search Controller with MongoDB Seed Data

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,8 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,5 +20,14 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitiallizer.InitDb(app);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
 
 app.Run();
