00f46d6daac6d358faeffbf7d3d03f249695a514
Vignesh <joshivignesh@users.noreply.github.com>
2023-11-26 09:46:18 +0530

    Added AuctionService to fetch SearchService

--- src/SearchService/Data/DbInitiallizer.cs
+++ src/SearchService/Data/DbInitiallizer.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualBasic;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
@@ -20,18 +21,16 @@ public class DbInitiallizer
             .CreateAsync();
 
         var count = await DB.CountAsync<Item>();
-        if(count == 0) 
+        
+        using var scope = app.Services.CreateScope();
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        var items = await httpClient.GetItemsForSearchDb();
+        Console.WriteLine(items.Count + "returned from the Auction service");
+
+        if(items.Count > 0)
         {
-            Console.WriteLine("No data, will attempt to seed");
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-
-            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
-
             await DB.SaveAsync(items);
-            
-        }
-            
+        } 
     }
 
 }
