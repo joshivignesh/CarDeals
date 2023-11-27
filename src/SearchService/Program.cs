411615a02aedb7380d3c3ed8cbc3a161f68c4d32
Vignesh <joshivignesh@users.noreply.github.com>
2023-11-27 19:59:25 +0530

    Added Poly and app Lifetime

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Models;
 using SearchService.Services;
@@ -9,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -21,7 +24,8 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async ()=>  {
+     try
 {
     await DbInitiallizer.InitDb(app);
 }
@@ -29,5 +33,12 @@ catch (Exception ex)
 {
     Console.WriteLine(ex);
 }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+ => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound )
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(5));
