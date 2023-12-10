31ec94af638eca4a4f2d10905567aa63f708c0cd
Vignesh <joshi.vignesh@gmail.com>
2023-12-10 21:47:37 +0530

    CreatedMap

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -3,6 +3,8 @@ using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,12 +17,15 @@ public class AuctionsController : ControllerBase
     private readonly AuctionDbContext _context;
 
     private readonly IMapper _mapper;
+    private readonly IPublishEndpoint _publishEndpoint;
 
     
-    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    public AuctionsController(AuctionDbContext context, IMapper mapper, 
+                    IPublishEndpoint publishEndpoint)
     {
         _context = context;
         _mapper = mapper;
+        _publishEndpoint = publishEndpoint;
     }
 
     [HttpGet]
@@ -59,13 +64,18 @@ public class AuctionsController : ControllerBase
         _context.Auctions.Add(auction);
 
         var result = await _context.SaveChangesAsync() > 0;
+
+        var newAuction = _mapper.Map<AuctionDto>(auction);
+
+        await _publishEndpoint.Publish(_mapper.Map<AuctionCreated>(newAuction));
+
         if(!result)
         {
             return BadRequest("Could not save changes to the DB");
         }
 
         return CreatedAtAction(nameof(GetAuctionsById),
-        new {auction.Id},_mapper.Map<AuctionDto>(auction));
+        new {auction.Id},newAuction);
     }
 
     [HttpPut("{Id}")]
